2cc44e088a9118ef9a087ac064cf00a51f805f96
Cagri Acar <cagriacar22@gmail.com>
2021-09-20 02:12:41 +0300

    Proje tamamlandı.

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ const getIpAPI = async () => {
     const data = await  response.json();
     return data;
 }
-const getIpInfoAPI = async () => {
-    const response =  await fetch(`https://ipinfo.io/161.185.160.93/geo/`);
+const getIpInfoAPI = async (ip) => {
+    const response =  await fetch(`https://ipinfo.io/${ip}/geo/`);
     if (response.status !== 200) {
         throw new Error("IP Sorgulanamıyor!!!");
     }
@@ -35,9 +35,64 @@ const getIpAddress = () => {
     });
 }
 const getIpInfo = () => {
-   /*  const ipAddress = document.getElementById('ip-adres');
-    let ip = ipAddress.textContent; */
-  
+    const ipAddress = document.getElementById('ip-adres');
+    let ip = ipAddress.textContent;
+    getIpInfoAPI(ip)
+    .then((data)=> {
+        console.log(data);
+        details.innerHTML = `
+        <table>
+                    <thead>
+                        <tr>
+                            <th>Bilgilendirme</th>
+                            <th>Açıklama</th>
+                          </tr>
+                      </thead>
+                      <tbody>
+                          <tr>
+                            <td>IP</td>
+                            <td>: ${data.ip}</td>
+                        </tr>
+                        <tr>
+                            <td>Hostname</td>
+                            <td>: ${data.hostname}</td>
+                        </tr>
+                        <tr>
+                            <td>Şehir</td>
+                            <td>: ${data.city}</td>
+                        </tr>
+                        <tr>
+                            <td>Bölge</td>
+                            <td>: ${data.region}</td>
+                            
+                        </tr>
+                        <tr>
+                            <td>Ülke</td>
+                            <td>: ${data.country}</td>
+                        </tr>
+                        <tr>
+                            <td>Lok- Koordinat</td>
+                            <td>: ${data.loc}</td>
+                        </tr>
+                        <tr>
+                            <td>Posta Kodu</td>
+                            <td>: ${data.postal}</td>
+                        </tr>
+                        <tr>
+                            
+                            <td>Zaman Dilimi</td>                            
+                            <td>: ${data.timezone}</td>                            
+                        </tr>
+                    </tbody>
+                  </table>
+        `;
+        
+    })
+    .catch((err)=>{
+        console.log('HATA : ', err.message)
+        const hata = document.querySelector('.hata');
+        hata.style.display = "block";
+    });
 }
 
 btnIp.addEventListener('click',getIpAddress)
@@ -47,17 +102,3 @@ btnInfo.addEventListener('click',getIpInfo)
 /* <p class="ip">IP Adresiniz : <span id="ip-adres">192.168.123</span></p> */
 
 
-
-
-    fetch("https://ipinfo.io/161.185.160.93/geo")
-.then((response)=>{
-console.log("Resolved : ",response);
-return response.json();
-})
-.then((data)=>{
-console.log(data)
-})
-.catch((err)=>{
-  console.log("Rejected : ",err)
-
-});
